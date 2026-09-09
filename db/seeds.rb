e613b0bc29bda2ac16d0dcc31e74b1bd5ddddb41
苺檸檬 <fraise.02.tarte@gmail.com>
2026-09-09 21:07:49 +0900

    rubocopの導入

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,6 +11,6 @@
 10.times do
     Article.create(
         title: Faker::Lorem.sentence(word_count: 5),
-        content: Faker::Lorem.sentence(word_count: 100) 
+        content: Faker::Lorem.sentence(word_count: 100)
     )
 end
